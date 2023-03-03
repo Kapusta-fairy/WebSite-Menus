5a84748712fd88e221ca2f45aa17afd9378e8bea
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-03 12:59:24 +0300

    change project name

--- up_trader/up_trader/asgi.py
+++ up_trader/up_trader/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for menus project.
+ASGI config for up_trader project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'up_trader.settings')
 
 application = get_asgi_application()
--- up_trader/up_trader/wsgi.py
+++ up_trader/up_trader/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for menus project.
+WSGI config for up_trader project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'up_trader.settings')
 
 application = get_wsgi_application()
