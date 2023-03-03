6a15224056190b0b855d21fd8df40b3c27671dcb
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-03 14:30:51 +0300

    fix AdditionalMenu

--- up_trader/additional_menus/models.py
+++ up_trader/additional_menus/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class AdditionalMenu(models.Model):
-    menu = models.ForeignKey('Menu', on_delete=models.CASCADE,
+    menu = models.ForeignKey('menus.Menu', on_delete=models.CASCADE,
                              verbose_name='меню', default=None)
     page_name = models.CharField(max_length=200, verbose_name='имя страницы')
 
