3c07419e9bd454d80528449401c8a6a0ecfb8d69
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-03 13:30:03 +0300

    create AdditionalMenu model

--- up_trader/additional_menus/models.py
+++ up_trader/additional_menus/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AdditionalMenu(models.Model):
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE,
+                             verbose_name='меню', default=None)
+    page_name = models.CharField(max_length=200, verbose_name='имя страницы')
+
+    def __str__(self):
+        return self.menu
+
+    class Meta:
+        verbose_name = 'доп. меню'
+        verbose_name_plural = 'доп. меню'
