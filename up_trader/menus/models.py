08f40eab0d3bf23bb8046a323848bd6fbf319c51
Kapusta-fairy <dknenorm63@gmail.com>
2023-03-03 13:18:57 +0300

    create Menu model

--- up_trader/menus/models.py
+++ up_trader/menus/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=150, verbose_name='имя')
+    slug = models.SlugField(max_length=20, unique=True, db_index=True,
+                            verbose_name="slug")
+    dependence = models.ForeignKey('Menu', on_delete=models.CASCADE,
+                                   verbose_name='зависимость', default=None)
+    url = models.URLField(max_length=200, verbose_name='ссылка')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'меню'
+        verbose_name_plural = 'меню'
